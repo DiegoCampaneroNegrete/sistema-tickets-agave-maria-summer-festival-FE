5554968cfccb967d3feebbb78aef127c5f1e7caa
Negrete, Diego <Diego.Negrete@united.com>
2026-03-29 20:14:36 -0600

    create constants file

---
+++ app/utils/constants.ts
@@ -0,0 +1,14 @@
+export const LABELS = {
+  appName: 'Agave Maria',
+  pos: 'POS',
+  dashboard: 'Dashboard',
+  cart: 'Carrito',
+  total: 'Total',
+  checkout: 'COBRAR',
+  empty: 'Vacío',
+  export: 'Exportar ventas',
+  totalSales: 'Ventas totales',
+  orders: 'Órdenes',
+  products: 'Productos vendidos',
+  remove: 'Eliminar'
+}
