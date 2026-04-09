29bb37bb18244af7a92e1b7ca61ac60cd522633e
Negrete, Diego <Diego.Negrete@united.com>
2026-04-09 16:50:20 -0600

    Add icons and assets  to the app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,24 @@ const config: CapacitorConfig = {
   appName: 'sistema-tickets-agave-maria-summer-festival',
   // webDir: 'public'
   webDir: 'out',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
